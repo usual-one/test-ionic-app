aa7a4761c3f7d38a218d467d7d7f9a8d9449c68a
Alexander Kostarev <kostarev.a.p@gmail.com>
2020-09-27 03:22:55 +0300

    init commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import * as io from 'socket.io-client';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,53 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  @ViewChild('content') private content: any;
 
+  public userName: string;
+
+  public currentMessage: string;
+  
+  public replies: object[];
+
+  private socket: SocketIOClient.Socket;
+
+  constructor() {
+    this.userName = 'usual_one';
+    this.currentMessage = "";
+    this.replies = [];
+    this.socket = io.connect('http://89.178.239.84:8888');
+  }
+
+  ngOnInit() {
+    this.socket.on('connect', () => {});
+
+    this.socket.on('messageToClient', reply => {
+      this.receive(reply); 
+    });
+  }
+
+  public scrollToBottom(): void {
+    this.content.scrollToBottom(300);
+  }
+
+  public send(): void {
+    this.sendMessage(this.currentMessage);
+    this.currentMessage = "";
+  }
+
+  public sendMessage(_message: string): void {
+    if (!_message) {
+      return;
+    }
+    const reply = {
+      nickname: this.userName,
+      message: _message
+    };
+    this.socket.emit('messageToServer', reply);
+    this.scrollToBottom();
+  }
+
+  public receive(reply): void {
+    this.replies.push(reply);
+  }
 }
